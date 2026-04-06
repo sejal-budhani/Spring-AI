7c6fb9a8a18c568f71155629c0aad5ad1f4c5608
sejal-budhani <sejalbudhani@gmail.com>
2026-04-06 23:22:42 +0530

    Added RAG based implementation

--- memory-app/src/main/java/com/example/advisors/config/AiConfig.java
+++ memory-app/src/main/java/com/example/advisors/config/AiConfig.java
@@ -8,6 +8,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,12 @@ import java.util.List;
 
 @Configuration
 public class AiConfig {
+
+//    By default, the maximum no of messages that gets stored for a conversation id is 20, to modify it, we do the below code
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().chatMemoryRepository(jdbcChatMemoryRepository).maxMessages(10).build();
+    }
 
     private Logger logger = LoggerFactory.getLogger(AiConfig.class);
 
